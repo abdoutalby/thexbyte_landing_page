08c882d9916c43670f4a9dedd862d989777c7d7f
abdoutalby <abderrahmentalby@gmail.com>
2025-11-06 09:15:10 +0100

    Initial commit with Astro setup for GitHub Pages

---
+++ astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://abdoutalby.github.io',
+  base: '/thexbyte_landing_page', // Replace with your repo name
+  integrations: []
+});
+
+//https://github.com/abdoutalby/thexbyte_landing_page
