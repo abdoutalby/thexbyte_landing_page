a3f666d8bd73a0d6b0e1cc22d18ec4aed1babe45
abdoutalby <abderrahmentalby@gmail.com>
2025-11-06 10:18:58 +0100

    Fix seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,8 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://abdoutalby.github.io',
-  base: '/thexbyte_landing_page', // Replace with your repo name
-  integrations: [],
+  site: 'https://thexbyte.com.tn',
+   integrations: [],
   build: {
     format: 'directory'
   }
